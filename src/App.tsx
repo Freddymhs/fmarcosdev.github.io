ed33a0819b2ff8d09b705ee2de26411dfe502799
fmarcosdev <fmarcosdev@gmail.com>
2025-04-09 16:27:55 -0400

    chore: added, personalization theme tailwind-v4

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Navigate, Route, Routes } from "react-router";
-import "./App.css";
 import MainLayout from "./components/templates/main-layout/MainLayout";
 import BlogLayout from "./components/templates/blog-layout/BlogLayout";
 import {
